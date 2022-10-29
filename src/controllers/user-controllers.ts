c0db01f936c38a85314fc270c15d14d39c97672b
RomanPantya <romanpantya@gmail.com>
2022-10-30 00:48:28 +0300

    user-controller create completed

---
+++ src/controllers/user-controllers.ts
@@ -0,0 +1,22 @@
+import { validate } from 'class-validator';
+import { Request, Response, NextFunction } from 'express';
+import { User, UserModel } from '../models/user-model';
+
+export async function createUser(req: Request, res: Response, next: NextFunction) {
+    const validateUser = new User();
+
+    validateUser.name = req.body.name;
+    validateUser.email = req.body.email;
+    validateUser.password = req.body.password;
+
+    const error = await validate(validateUser);
+
+    if (error.length > 0) {
+        return next(new Error(error.join(' | ')));
+    }
+
+    const user = new UserModel(validateUser);
+
+    const created = await user.save();
+    return res.json(created);
+}
