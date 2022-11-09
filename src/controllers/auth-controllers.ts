d224233c659cb745dc45cfab644cd50df175d714
RomanPantya <romanpantya@gmail.com>
2022-11-09 02:52:56 +0200

    fixed login

--- src/controllers/auth-controllers.ts
+++ src/controllers/auth-controllers.ts
@@ -1,20 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
 import { sign } from 'jsonwebtoken';
 import { AuthDto } from '../dto/auth.dto';
 import { UserModel } from '../models/user-model';
 
 export async function login(req:Request, res: Response, next: NextFunction) {
-    const validateAuth = new AuthDto();
+    const data = req.body;
 
-    validateAuth.email = req.body.email;
-    validateAuth.password = req.body.password;
+    const validateAuth = plainToInstance(AuthDto, data);
 
     const error = await validate(validateAuth);
-
     if (error.length > 0) {
         return next(new Error(error.join(' | ')));
     }
+
     const user = await UserModel.findOne({ email: validateAuth.email });
     if (!user) {
         return next(new Error('error 403: email is not valid!'));
@@ -25,7 +25,7 @@ export async function login(req:Request, res: Response, next: NextFunction) {
 
     const twoTokens = {
         access: sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '2h' }),
-        refresh: sign({ id: user.id }, process.env.JWT_SECRET!, { expiresIn: '2d' }),
+        refresh: sign({ id: user.id }, process.env.JWT_SECRET_REF!, { expiresIn: '2d' }),
     };
 
     return res.json({ twoTokens });
